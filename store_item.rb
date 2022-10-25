dca9e525bab644dd3c226e3c4520cc4f4dce76bd
Sara Gluck <srfgglck@gmail.com>
2022-10-25 14:06:11 -0500

    Changes comment

---
+++ store_item.rb
@@ -0,0 +1,9 @@
+#Using JavaScript syntax
+backpack1 = { color: "red", pattern: "polka dots", price: "35" }
+backpack2 = { color: "blue", pattern: "stripes", price: "25" }
+backpack3 = { color: "yellow", pattern: "bumblebees", price: "30" }
+
+#Using Ruby syntax
+backpack1 = { :color => "red", :pattern => "polka dots", :price => "35" }
+backpack2 = { :color => "blue", :pattern => "stripes", :price => "25" }
+backpack3 = { :color => "yellow", :pattern => "bumblebees", :price => "30" }
